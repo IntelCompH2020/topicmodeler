4779695db87e0e6203e04243ebbacdb27a38f32b
Lorena Calvo Bartolomé <100330717@alumnos.uc3m.es>
2022-07-26 19:09:38 +0200

    Updated script for generating embeddings.

--- aux/create_embeddings.py
+++ aux/create_embeddings.py
@@ -1,15 +1,15 @@
 import argparse
 import warnings
 from pathlib import Path
-from typing import List
-import pandas as pd
-import dask.dataframe as dd
-from langdetect import detect
-import numpy as np
-from sentence_transformers import SentenceTransformer
-from nltk.tokenize import sent_tokenize
-from tqdm import tqdm
 from time import gmtime, strftime
+from typing import List
+
+import numpy as np
+import pandas as pd
+from langdetect import detect
+from sentence_transformers import SentenceTransformer
+from tqdm import tqdm
+
 
 class EmbeddingsManager(object):
 
@@ -19,16 +19,11 @@ class EmbeddingsManager(object):
 
         Parameters
         ----------
-        max_seq_length : 
-        texts:
-
-        Returns
-        -------
-        allWdLists : Dictionary (path -> dictionary)
-            One dictionary entry per wordlist
-            key is the absolute path to the wordlist
-            value is a dictionary with metadata
-        """        
+        max_seq_length: int
+            Context of the transformer model used for the embeddings generation
+        texts: list[str]
+            The sentences to embed
+        """
 
         max_local_length = np.max([len(t.split()) for t in texts])
         if max_local_length > max_seq_length:
@@ -43,14 +38,19 @@ class EmbeddingsManager(object):
 
         Parameters
         ----------
-        texts : 
-        sbert_model_to_load:
-        batch_size:
-        max_seq_length:
-        
+        texts : list[str]
+            The sentences to embed
+        sbert_model_to_load: str
+            Model (e.g. paraphrase-distilroberta-base-v1) to be used for generating the embeddings
+        batch_size: int (default=32)
+            The batch size used for the computation
+        max_seq_length: int
+            Context of the transformer model used for the embeddings generation
+
         Returns
         -------
-        embeddings : 
+        embeddings: list
+            List with the embeddings for each document
         """
 
         model = SentenceTransformer(sbert_model_to_load)
@@ -59,17 +59,32 @@ class EmbeddingsManager(object):
             model.max_seq_length = max_seq_length
 
         self._check_max_local_length(max_seq_length, texts)
-        embeddings = model.encode(texts, show_progress_bar=True, batch_size=batch_size).tolist()
-     
+        embeddings = model.encode(
+            texts, show_progress_bar=True, batch_size=batch_size).tolist()
+
         return embeddings
 
-    def add_embeddins_to_parquet(self, parquet_file: Path, parquet_new: Path, embeddins_model: str, max_seq_length: int) -> Path:
+    def add_embeddins_to_parquet(self, parquet_file: Path, parquet_new: Path, embeddins_model: str, max_seq_length: int, source: str) -> Path:
+        """Generates the embeddings for a set of files given in parquet format, and saves them in a new parquet file that containing the original data plus an additional column named 'embeddings'.
+
+        Parameters
+        ----------
+        parquet_file : Path
+            Path from which the source parquet files are read
+        parquet_new: Path
+            Path in which the new parquet files will be saved
+        embeddins_model: str
+            Model to be used for generating the embeddings
+        max_seq_length: int
+            Context of the transformer model used for the embeddings generation
+        source: str
+        """
 
         path_parquet = Path(parquet_file)
 
         res = []
         for entry in path_parquet.iterdir():
-            # check if it a file
+            # check if it is a file
             if entry.as_posix().endswith("parquet"):
                 res.append(entry)
 
@@ -80,19 +95,39 @@ class EmbeddingsManager(object):
                 lang = 'Other'
             return lang
 
+        if source == "scholar":
+            raw_text_fld = "paperAbstract"
+            title_fld = "title"
+        elif source == "cordis":
+            raw_text_fld = "rawtext"
+            title_fld = "title"
+        elif source == "patents":
+            pass
+
         for f in tqdm(res):
-            df = pd.read_parquet(f)
-            df = df[df.paperAbstract != ""]
-            df['langue'] = df['paperAbstract'].apply(det)
-            df = df[df.langue == 'en']
-            df['title_abstract'] = df[["title", "paperAbstract"]].apply(" ".join, axis=1)
-            raw = df.title_abstract.values.tolist()
+            df = pd.read_parquet(f).fillna("")
+
+            # Filter out abstracts with no text
+            df = df[df[raw_text_fld] != ""]
+
+            # Detect abstracts' language and filter out those which are not in English
+            df['langue'] = df[raw_text_fld].apply(det)
+            df = df[df['langue'] == 'en']
+
+            # Concatenation of title and abstract is used as the text to generated of the embeddings
+            df['title_abstract'] = df[[title_fld, raw_text_fld]].apply(
+                " ".join, axis=1)
+            raw = df['title_abstract'].values.tolist()
             embeddings = self.bert_embeddings_from_list(
-                    texts=raw, 
-                    sbert_model_to_load=embeddins_model, 
-                    max_seq_length=max_seq_length)
+                texts=raw,
+                sbert_model_to_load=embeddins_model,
+                max_seq_length=max_seq_length)
+
+            # Remove unnecessay columns in the df
             df.drop('title_abstract', inplace=True, axis=1)
             df['embeddings'] = embeddings
+
+            # Save new df in parquet file
             time = strftime("_%Y-%m-%d-%-S", gmtime())
             new_name = "parquet_embeddings" + time + ".parquet"
             outFile = parquet_new.joinpath(new_name)
@@ -101,13 +136,26 @@ class EmbeddingsManager(object):
 
         return
 
+
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Scripts for Embeddings Service")
-    parser.add_argument("--path_parquet", type=str, default=None, required=True, metavar=("path_to_parquet"), help="path to parquet file to caclulate embeddings of")
-    parser.add_argument("--path_new", type=str, default=None, required=True, metavar=("path_new"), help="path to parquet folder to locate embeddings")
-    parser.add_argument("--embeddings_model", type=str, default="all-mpnet-base-v2", required=False, metavar=("embeddings_model"), help="Model to be used for calculating the embeddings")
-    parser.add_argument("--max_sequence_length", type=int, default=384, required=False, metavar=("max_sequence_length"), help="Model's context")
+    parser = argparse.ArgumentParser(
+        description="Scripts for Embeddings Service")
+    parser.add_argument("--path_parquet", type=str, default=None,
+                        required=True, metavar=("path_to_parquet"),
+                        help="path to parquet file to caclulate embeddings of")
+    parser.add_argument("--source", type=str, default=None,
+                        required=True, metavar=("source"),
+                        help="Key of the source dataset for which the embeddings are being calculated, 'cordis', 'scholar', 'patents'.")
+    parser.add_argument("--path_new", type=str, default=None,
+                        required=True, metavar=("path_new"),
+                        help="path to parquet folder to locate embeddings")
+    parser.add_argument("--embeddings_model", type=str,
+                        default="all-mpnet-base-v2", required=False,
+                        metavar=("embeddings_model"),
+                        help="Model to be used for calculating the embeddings")
+    parser.add_argument("--max_sequence_length", type=int, default=384,
+                        required=False, metavar=("max_sequence_length"), help="Model's context")
 
     args = parser.parse_args()
 
@@ -115,5 +163,5 @@ if __name__ == "__main__":
 
     parquet_path = Path(args.path_parquet)
     parquet_new = Path(args.path_new)
-    em.add_embeddins_to_parquet(parquet_path, parquet_new, args.embeddings_model, args.max_sequence_length)
-
+    em.add_embeddins_to_parquet(
+        parquet_path, parquet_new, args.embeddings_model, args.max_sequence_length, args.source)
