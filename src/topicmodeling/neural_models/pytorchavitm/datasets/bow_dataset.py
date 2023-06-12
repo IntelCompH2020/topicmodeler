Disabling similarities calculation and fixing some bugs in ProdLDA.

src/topicmodeling/neural_models/pytorchavitm/datasets/bow_dataset.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import torch
 from torch.utils.data import Dataset
-
+from scipy import sparse
 
 class BOWDataset(Dataset):
     
@@ -26,10 +26,13 @@ class BOWDataset(Dataset):
 
     def __len__(self):
         """Returns length of dataset."""
-        return len(self.X)
+        return self.X.shape[0]
 
     def __getitem__(self, i):
         """Returns sample from dataset at index i."""
-        X = torch.FloatTensor(self.X[i])
-
+        if type(self.X[i]) == sparse.csr_matrix:
+            X = torch.FloatTensor(self.X[i].todense())
+        else:
+            X = torch.FloatTensor(self.X[i])
+        
         return {'X': X}

src/topicmodeling/neural_models/pytorchavitm/utils/data_preparation.py
@@ -47,7 +47,7 @@ def prepare_dataset(corpus, val_size=0.25):
                        for i in np.arange(len(docs_train))]
 
     # Learn the vocabulary dictionary, train_bow = document-term matrix.
-    train_bow = cv.fit_transform(docs_train_conv).toarray()
+    train_bow = cv.fit_transform(docs_train_conv)#.toarray()
 
     # Array mapping from feature integer indices to feature name.
     idx2token = cv.get_feature_names_out()
@@ -62,7 +62,7 @@ def prepare_dataset(corpus, val_size=0.25):
     ##############################################
     docs_val_conv = [" ".join(docs_val[i]) for i in np.arange(len(docs_val))]
     val_bow = cv.transform(docs_val_conv)
-    val_bow = val_bow.toarray()
+    val_bow = val_bow#.toarray()
     val_data = BOWDataset(val_bow, idx2token, cv)
 
     return train_data, val_data, input_size, id2token, docs_train
@@ -89,7 +89,7 @@ def prepare_hold_out_dataset(hold_out_corpus, cv, idx2token):
     docs_ho_conv = \
         [" ".join(hold_out_corpus[i]) for i in np.arange(len(hold_out_corpus))]
     ho_bow = cv.transform(docs_ho_conv)
-    ho_bow = ho_bow.toarray()
+    ho_bow = ho_bow#.toarray()
     ho_data = BOWDataset(ho_bow, idx2token, cv)
 
     return ho_data